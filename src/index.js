c519b5731b563d2bda43889e035755d526b34198
Jaider-Duvan <jaiderduvan961@gmail.com>
2024-06-02 14:12:00 -0500

    Se crea menu y seccion de servicios

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 /* -------------------------- Importar dependencias ------------------------- */
-import  express from 'express'
+import express from 'express'
 
-import {join, dirname} from 'path' // identifica una direccion especifica para concatenar info
-import {fileURLToPath} from 'url'
-import {engine} from 'express-handlebars'
+import { join, dirname } from 'path' // identifica una direccion especifica para concatenar info
+import { fileURLToPath } from 'url'
+import { engine } from 'express-handlebars'
 import morgan from 'morgan';
 /* ----------------------------- Inicialización ----------------------------- */
 const app = express();
@@ -13,7 +13,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set('port', process.env.PORT || 3000);
 app.set('views', join(__dirname, 'views'));
 /* ---------------- Configuracion del manejador de plantillas --------------- */
-app.engine('.hbs',engine({
+app.engine('.hbs', engine({
     defaultLayout: 'main',
     layoutsDir: join(app.get('views'), 'layouts'),
     partialsDir: join(app.get('views'), 'partials'),
@@ -24,18 +24,17 @@ app.set('view engine', '.hbs')
 /* ------------------------------ Middlewares ------------------------------ */
 
 app.use(morgan('dev')); // conocer las peticiones 
-app.use(express.urlencoded({extended: false})); // decodificar las peticiones de la url
+app.use(express.urlencoded({ extended: false })); // decodificar las peticiones de la url
 app.use(express.json()); // Usa informacion en formato Json
 
 /* --------------------------------- Routes --------------------------------- */
-app.get('/inicio', (req, res)=>{
-    res.json({"message" : "Hola Mundo"})
+app.get('/', (req, res) => {
+    res.render('index')
 });
 /* ------------------------------ Public files ------------------------------ */
 app.use(express.static(join(__dirname, 'public')));
 
 /* ------------------------------- Run Server ------------------------------- */
-app.listen(app.get('port'), ()=>{
+app.listen(app.get('port'), () => {
     console.log('Server listening on port', app.get('port'))
 })
-
